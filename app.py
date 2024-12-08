8f70c67cd99b7830199ef901f2e63fdad20d25d6
Daniel Samuel <danielsamuel@Daniels-MacBook-Pro.local>
2024-12-08 05:35:52 +0200

    first commit

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def video_feed():
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5001, debug=True,allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=7017, debug=True,allow_unsafe_werkzeug=True)
